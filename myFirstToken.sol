e178ec570087bd107f22e6ff6786f2ebe3ac41f7
codemonk22 <105297413+codemonk22@users.noreply.github.com>
2022-05-10 09:10:06 -0500

    Add files via upload

---
+++ myFirstToken.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.4.24;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+//import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+// //contract SampleToken is ERC20Detailed, ERC20 {
+//     contract SampleToken is ERC20 {
+
+//     constructor(string memory _name, string memory _symbol, uint8 _decimals, uint _initialSupply) 
+//     ERC20(_name, _symbol, _decimals) public {
+//         require(_initialSupply > 0, "INITIAL_SUPPLY has to be greater than 0");
+//         _mint(msg.sender, _initialSupply);
+//     }
+// }
+
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+//pragma solidity ^0.8.0;
+
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GLDToken is ERC20 {
+    //constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+    constructor(uint256 initialSupply) ERC20() {
+        _mint(msg.sender, initialSupply);
+    }
+}
